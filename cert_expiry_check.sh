203750e7aa226bd2085a93e2aef29f0a6feab4db
jl-repo <jaredaleslie@gmail.com>
2022-06-06 15:19:13 +1000

    new script

---
+++ cert_expiry_check.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+gracedays=30
+
+# Colour variables.
+RED='\e[0;91m'
+GREEN='\e[0;92m'
+YELLOW='\e[43m'
+CLEAR='\e[0m'
+
+#URL lists
+declare -a servers=(
+    "google.com"
+    "shellcheck.net"
+    "expired.badssl.com"
+)
+
+echo -e "Checking certificates on sites with port 443."
+echo -e "Grace days is set to $gracedays days."
+echo -e ""
+for server in "${servers[@]}"; do
+        #Capture expiry date using openssl on port 443.
+        data="$(date --date="$(echo | openssl s_client -connect "${server}":443 -servername "${server}" 2>/dev/null | openssl x509 -noout -enddate | awk -F '=' '{print $NF}' )" --iso-8601)"
+        #Math to compare and generate date difference
+        ssldate="$(date -d "${data}" '+%s')"
+        nowdate="$(date '+%s')"
+        diff="$(("$ssldate"-"$nowdate"))"
+        #If statement tests to determine if a cert is expired or expring based on the gracedays.
+        if test "${diff}" -lt "$(("${gracedays}"*24*3600))"; then
+            if test "${diff}" -lt "0"; then
+                echo -e "$RED""The certificate for ${server} has already expired.""$CLEAR"
+            else
+                echo -e "$YELLOW""The certificate for ${server} will expire in $(("$diff"/3600/24)) days.""$CLEAR"
+            fi
+        else
+            echo -e "$GREEN""The certifcate for ${server} will expire in $(("$diff"/3600/24)) days.""$CLEAR"
+        fi
+done
