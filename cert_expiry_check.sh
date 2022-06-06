efa3b5033e77adebe00a4a9d08f607c925c4c964
jl-repo <jaredaleslie@gmail.com>
2022-06-06 16:37:55 +1000

    Updated declare to include ports

--- cert_expiry_check.sh
+++ cert_expiry_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-gracedays=30
+
 
 # Colour variables.
 RED='\e[0;91m'
@@ -7,20 +7,25 @@ GREEN='\e[0;92m'
 YELLOW='\e[43m'
 CLEAR='\e[0m'
 
-#URL lists
+#Grace days before expiry.
+gracedays=30
+
+#URL lists with port to check.
+
 declare -a servers=(
-    "google.com"
-    "shellcheck.net"
-    "expired.badssl.com"
+    "google.com:443"
+    "shellcheck.net:443"
+    "expired.badssl.com:443"
 )
 
-echo -e "Checking certificates on sites with port 443."
+#Certificate expiry.
+echo -e "Checking certificates..."
 echo -e "Grace days is set to $gracedays days."
 echo -e ""
 for server in "${servers[@]}"; do
         #Capture expiry date using openssl on port 443.
-        data="$(date --date="$(echo | openssl s_client -connect "${server}":443 -servername "${server}" 2>/dev/null | openssl x509 -noout -enddate | awk -F '=' '{print $NF}' )" --iso-8601)"
-        #Math to compare and generate date difference
+        data="$(date --date="$(echo | openssl s_client -connect "${server}" -servername "${server}" 2>/dev/null | openssl x509 -noout -enddate | awk -F '=' '{print $NF}' )" --iso-8601)"
+        #Math to compare and generate date difference.
         ssldate="$(date -d "${data}" '+%s')"
         nowdate="$(date '+%s')"
         diff="$(("$ssldate"-"$nowdate"))"
@@ -28,7 +33,7 @@ for server in "${servers[@]}"; do
         if test "${diff}" -lt "$(("${gracedays}"*24*3600))"; then
             if test "${diff}" -lt "0"; then
                 echo -e "$RED""The certificate for ${server} has already expired.""$CLEAR"
-            else
+            els
                 echo -e "$YELLOW""The certificate for ${server} will expire in $(("$diff"/3600/24)) days.""$CLEAR"
             fi
         else
