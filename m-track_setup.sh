c35ff7a68b5c897be4ce65e5a78220bc18681e90
jl-repo <jaredaleslie@gmail.com>
2022-09-09 09:53:29 +1000

    Sepeated NGINX to be optional.

--- m-track_setup.sh
+++ m-track_setup.sh
@@ -2,7 +2,7 @@
 # By Jared Leslie
 # Email: jared.leslie@quinticon.com.au
 # Date: 08/09/2022
-# Version 1.0
+# Version 1.2
 # Quinticon M-Track Deployment Script. 
 # Works with both Debian and Fedora based distributions. Script validated on Ubuntu Server 22.04 and Redhat Enterprise Linux 8 and 9.
 
@@ -48,47 +48,32 @@ else
     exit 1
 fi
 
-# User Input Variables.
-echo ""
-echo "Enter the email for the SSL CSR (Default: person@quinticon.com.au). <Enter> to finalize."
-read -r EMAIL
-EMAIL="${EMAIL:=person@quinticon.com.au}"
-echo "Enter the URL for the SSL CSR (Default: mtrack.example.com). <Enter> to finalize."
-read -r COMMONNAME
-COMMONNAME="${COMMONNAME:=mtrack.example.com}"
-echo "Enter the country code for the SSL CSR (Default: AU). <Enter> to finalize."
-read -r COUNTRY
-COUNTRY="${COUNTRY:=AU}"
-echo "Enter the state for the SSL CSR (Default: New South Wales). <Enter> to finalize."
-read -r STATE
-STATE="${STATE:=New South Wales}"
-echo "Enter the locality for the SSL CSR (Default: Sydney). <Enter> to finalize."
-read -r LOCALITY
-LOCALITY="${LOCALITY:=Sydney}"
-echo "Enter the organization for the SSL CSR (Default: Quinticon Pty Ltd). <Enter> to finalize."
-read -r ORGANIZATION
-ORGANIZATION="${ORGANIZATION:=Quinticon Pty Ltd}"
-echo "Enter the organizationalunit for the SSL CSR (Default: Development). <Enter> to finalize."
-read -r ORGANIZATIONALUNIT
-ORGANIZATIONALUNIT="${ORGANIZATIONALUNIT:=Development}"
-echo ""
+# Case statement for Nginx Optional Install.
+while true; do
+    read -r -p 'Do you want to install and configure NGINX as a reverse proxy? Yes/No ' continue
+    case "$continue" in
+    n | N | no | No) NGINX=no break ;;
+    y | Y | yes | Yes) NGINX=yes break ;;
+    *) /usr/bin/echo -e "Response not valid" ;;
+    esac
+done
 
-# Install Prereq Software.
+# Install M-Track Prereq Software.
 if [ "${PKMGR}" = yum ]; then
     echo "Adding neo4j repository..."
     rpm --import https://debian.neo4j.com/neotechnology.gpg.key
     echo -e '[neo4j]\nname=Neo4j Yum Repo\nbaseurl=http://yum.neo4j.com/stable\nenabled=1\ngpgcheck=1' > /etc/yum.repos.d/neo4j.repo
     echo ""
-    echo "Installing OpenJava 11, Neo4j and Nginx..."
-    yum install neo4j-3.5.5 nginx openssl java-11-openjdk -y # using yum for wider compatibility. has a link to dnf for later distro's.
+    echo "Installing OpenJava 11 and Neo4j..."
+    yum install neo4j-3.5.5 openssl java-11-openjdk -y # using yum for wider compatibility. has a link to dnf for later distro's.
 elif [ "${PKMGR}" = apt ]; then
     echo "Adding neo4j repository..."
     curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | gpg --dearmor -o /usr/share/keyrings/neo4j.gpg
     echo "deb [signed-by=/usr/share/keyrings/neo4j.gpg] https://debian.neo4j.com stable 3.5" | tee -a /etc/apt/sources.list.d/neo4j.list
-    echo "Installing OpenJava 11, Neo4j and Nginx..."
+    echo "Installing OpenJava 11 and Neo4j..."
     add-apt-repository universe -y
     apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common neo4j nginx openssl openjdk-11-jdk openjdk-8-jdk
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common neo4j openssl openjdk-11-jdk openjdk-8-jdk
     systemctl stop neo4j # required as after installation the service starts causing issues with changing the initial password.
     rm -r /var/lib/neo4j/data/dbms/auth # required as after installation the service starts causing issues with changing the initial password.
 fi
@@ -103,68 +88,99 @@ systemctl enable --now neo4j
 echo ""
 systemctl status neo4j --no-pager
 
+if [ "${NGINX}" = yes ]; then
+    if [ "${PKMGR}" = yum ]; then
+        echo "Installing Nginx..."
+        yum install nginx -y # using yum for wider compatibility. has a link to dnf for later distro's.
+    elif [ "${PKMGR}" = apt ]; then
+        echo "Installing Nginx..."
+        apt-get update
+        apt-get install -y nginx
+    fi
+    echo ""
+    echo "Enter the email for the SSL CSR (Default: person@quinticon.com.au). <Enter> to finalize."
+    read -r EMAIL
+    EMAIL="${EMAIL:=person@quinticon.com.au}"
+    echo "Enter the URL for the SSL CSR (Default: mtrack.example.com). <Enter> to finalize."
+    read -r COMMONNAME
+    COMMONNAME="${COMMONNAME:=mtrack.example.com}"
+    echo "Enter the country code for the SSL CSR (Default: AU). <Enter> to finalize."
+    read -r COUNTRY
+    COUNTRY="${COUNTRY:=AU}"
+    echo "Enter the state for the SSL CSR (Default: New South Wales). <Enter> to finalize."
+    read -r STATE
+    STATE="${STATE:=New South Wales}"
+    echo "Enter the locality for the SSL CSR (Default: Sydney). <Enter> to finalize."
+    read -r LOCALITY
+    LOCALITY="${LOCALITY:=Sydney}"
+    echo "Enter the organization for the SSL CSR (Default: Quinticon Pty Ltd). <Enter> to finalize."
+    read -r ORGANIZATION
+    ORGANIZATION="${ORGANIZATION:=Quinticon Pty Ltd}"
+    echo "Enter the organizationalunit for the SSL CSR (Default: Development). <Enter> to finalize."
+    read -r ORGANIZATIONALUNIT
+    ORGANIZATIONALUNIT="${ORGANIZATIONALUNIT:=Development}"
+    echo ""
 
-# Create the CSR and Private Key request
-echo "Creating CSR and Private Key..."
-openssl req -newkey rsa:2048 -passout pass:$PASSWORD -keyout /etc/ssl/certs/mtrack.key -out /etc/ssl/certs/mtrack.csr -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATIONALUNIT/CN=$COMMONNAME/emailAddress=$EMAIL"
+	# Create the CSR and Private Key request
+	echo "Creating CSR and Private Key..."
+	openssl req -newkey rsa:2048 -passout pass:$PASSWORD -keyout /etc/ssl/certs/mtrack.key -out /etc/ssl/certs/mtrack.csr -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATIONALUNIT/CN=$COMMONNAME/emailAddress=$EMAIL"
 
-# Create a Self Signed Certificate
-echo "Creating an Self-Signed Certificate..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -passin pass:$PASSWORD -key /etc/ssl/certs/mtrack.key -out /etc/ssl/certs/mtrack.cert -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATIONALUNIT/CN=$COMMONNAME/emailAddress=$EMAIL"
+	# Create a Self Signed Certificate
+	echo "Creating an Self-Signed Certificate..."
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -passin pass:$PASSWORD -key /etc/ssl/certs/mtrack.key -out /etc/ssl/certs/mtrack.cert -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATIONALUNIT/CN=$COMMONNAME/emailAddress=$EMAIL"
 
-# Generate a OpenSSL DHPARAM file.
-echo "Generate an OpenSSL DHPARAM file..."
-openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+	# Generate a OpenSSL DHPARAM file.
+	echo "Generate an OpenSSL DHPARAM file..."
+	openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
-# Configure Nginx config file.
-cat > /etc/nginx/conf.d/${COMMONNAME}.conf << EOF
-server {
-    listen 80;
-    return 301 https://$host$request_uri;
-}
-server {
-    listen 443 http2 ssl;
-    listen [::]:443 http2 ssl;
-    server_name ${COMMONNAME};
-    ssl_certificate /etc/ssl/certs/mtrack.cert;
-    ssl_certificate_key /etc/ssl/certs/mtrack.key;
-    ssl_password_file /etc/ssl/certs/ssl_passwords.txt;
-    ssl_dhparam /etc/ssl/certs/dhparam.pem;
+	# Configure Nginx config file.
+	cat > /etc/nginx/conf.d/${COMMONNAME}.conf <<- EOF
+	server {
+		listen 80;
+		return 301 https://$host$request_uri;
+	}
+	server {
+		listen 443 http2 ssl;
+		listen [::]:443 http2 ssl;
+		server_name ${COMMONNAME};
+		ssl_certificate /etc/ssl/certs/mtrack.cert;
+		ssl_certificate_key /etc/ssl/certs/mtrack.key;
+		ssl_password_file /etc/ssl/certs/ssl_passwords.txt;
+		ssl_dhparam /etc/ssl/certs/dhparam.pem;
 
-    location / {
-        proxy_pass http://localhost:8080;
-        proxy_redirect http://localhost:8080 https://${COMMONNAME};
-    }
-}
-EOF
+		location / {
+			proxy_pass http://localhost:8080;
+			proxy_redirect http://localhost:8080 https://${COMMONNAME};
+		}
+	}
+	EOF
 
-# Create password file for nginx.
-cat > /etc/ssl/certs/ssl_passwords.txt << EOF
-${PASSWORD}
-EOF
-chmod 440 /etc/ssl/certs/ssl_passwords.txt
+	# Create password file for nginx.
+	cat > /etc/ssl/certs/ssl_passwords.txt <<- EOF
+	${PASSWORD}
+	EOF
+	chmod 440 /etc/ssl/certs/ssl_passwords.txt
 
-# Set SELinux Bool for nginx.
-if [ "${SELINUX_STATUS}" = SELINUX=enforcing ] || [ "${SELINUX_STATUS}" = SELINUX=permissive ]; then
-    echo "Setting SELinux configuration..."
-    setsebool -P httpd_can_network_connect 1
-else
-    echo "SELinux is disabled or not configured."
+	# Set SELinux Bool for nginx.
+	if [ "${SELINUX_STATUS}" = SELINUX=enforcing ] || [ "${SELINUX_STATUS}" = SELINUX=permissive ]; then
+		echo "Setting SELinux configuration..."
+		setsebool -P httpd_can_network_connect 1
+	else
+		echo "SELinux is disabled or not configured."
+	fi
+
+	echo "Enabing nginx for startup..."
+	systemctl enable --now nginx
+	echo ""
+	systemctl status nginx --no-pager
+	# Required for Debian based systems to pickup the new config where Nginx is already started.
+	if [ "${PKMGR}" = apt ]; then
+		echo "Restarting Nginx..."
+		systemctl restart nginx
+	fi
+	echo ""
+	systemctl status nginx
 fi
-
-
-
-echo "Enabing nginx for startup..."
-systemctl enable --now nginx
-echo ""
-systemctl status nginx --no-pager
-# Required for Debian based systems to pickup the new config where Nginx is already started.
-if [ "${PKMGR}" = apt ]; then
-    echo "Restarting Nginx..."
-    systemctl restart nginx
-fi
-echo ""
-systemctl status nginx
 
 # Set Java Version for Redhat systems.
 if [ "${PKMGR}" = yum ]; then
@@ -200,14 +216,22 @@ if [ "$LOCAL" != LANG=en_AU.UTF-8 ]; then
 fi
 
 # Setting firewall rules for M-Track.
-if [ "${FIREWALLD}" = running ] || [ "${PKMGR}" = yum ]; then
+if [ "${FIREWALLD}" = running ] || [ "${NGINX}" = yes ]; then
     echo "Adding Firewall Rules..."
     firewall-cmd --add-service=https --permanent
     echo "Reloading Firewalld"
     firewall-cmd --reload
-elif [ "${FIREWALLUFW}" = active ] || [ "${PKMGR}" = apt ]; then
+elif [ "${FIREWALLD}" = running ] || [ "${NGINX}" = no ]; then
+    echo "Adding Firewall Rules..."
+    firewall-cmd --add-port=8080 --permanent
+    echo "Reloading Firewalld"
+    firewall-cmd --reload
+elif [ "${FIREWALLUFW}" = active ] || [ "${NGINX}" = yes ]; then
     echo "Adding Firewall Rules..."
     ufw allow 443/tcp
+elif [ "${FIREWALLUFW}" = active ] || [ "${NGINX}" = no ]; then
+    echo "Adding Firewall Rules..."
+    ufw allow 8080/tcp
 else
     echo "Firewall not enabled."
 fi
